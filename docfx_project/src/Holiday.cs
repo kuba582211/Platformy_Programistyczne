a365c1c6cae40054534e1df12bb01cf4e796db22
kuba582211 <166310325+kuba582211@users.noreply.github.com>
2025-06-12 11:20:00 +0200

    Update Holiday.cs

--- docfx_project/src/Holiday.cs
+++ docfx_project/src/Holiday.cs
@@ -1,12 +1,1 @@
-﻿using System;
 
-namespace GoogleStyleCalendar
-{
-    public class Holiday
-    {
-        public DateTime Date { get; set; }
-        public string LocalName { get; set; }
-        public string Name { get; set; }
-        public string CountryCode { get; set; }
-    }
-}
